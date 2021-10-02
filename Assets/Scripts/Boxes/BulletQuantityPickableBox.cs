0bdcfc060b8638742aabff0045f962294fdcca06
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-10-02 19:28:28 -0300

    Implement bullet quantity and damage pickable box

---
+++ Assets/Scripts/Boxes/BulletQuantityPickableBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BulletQuantityPickableBox : PickableBox
+{
+    [SerializeField] private int quantityOfBullets = 1;
+    protected override void OnRegisterToPlayer()
+    {
+        base.OnRegisterToPlayer();
+        Shooter shooter = CachedPlayer.GetComponentInChildren<Shooter>();
+        shooter.QuantityOfBullets += quantityOfBullets;
+    }
+
+    protected override void OnUnregisterToPlayer()
+    {
+        base.OnUnregisterToPlayer();
+        Shooter shooter = CachedPlayer.GetComponentInChildren<Shooter>();
+        shooter.QuantityOfBullets -= quantityOfBullets;
+    }
+}
---
+++ Assets/Scripts/Boxes/DamagePickableBox.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class DamagePickableBox : PickableBox
+{
+    [SerializeField] private float DamageToAdd = 1.0f;
+    protected override void OnRegisterToPlayer()
+    {
+        base.OnRegisterToPlayer();
+        Shooter shooter = CachedPlayer.GetComponentInChildren<Shooter>();
+        shooter.BulletDamage += DamageToAdd;
+    }
+
+    protected override void OnUnregisterToPlayer()
+    {
+        base.OnUnregisterToPlayer();
+        Shooter shooter = CachedPlayer.GetComponentInChildren<Shooter>();
+        shooter.BulletDamage -= DamageToAdd;
+    }
+}
