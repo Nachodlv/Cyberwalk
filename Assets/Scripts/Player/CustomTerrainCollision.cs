93ec197da184d6d9d896d2fead01f9d27e985cca
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-10-03 20:40:12 -0300

    Add new levels

---
+++ Assets/Scripts/Player/CustomTerrainCollision.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class CustomTerrainCollision : MonoBehaviour
+{
+    private CharacterMovementController _controller;
+    private bool _leftCollision;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector2 myPosition = transform.position;
+        _controller = other.gameObject.GetComponent<CharacterMovementController>();
+        if (_controller)
+        {
+            Vector2 position = _controller.RigidBodyComp.position;
+            _leftCollision = (position - myPosition).x < 0;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        CharacterMovementController controller = other.gameObject.GetComponent<CharacterMovementController>();
+        if (controller)
+        {
+            _controller = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (_controller)
+        {
+            _controller.CollisionLeft = !_leftCollision;
+            _controller.CollisionRight = _leftCollision;
+        }
+    }
+}
