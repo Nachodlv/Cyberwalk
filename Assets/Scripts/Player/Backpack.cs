b15196a4bc4305464f10252970ce6ce2ee5e24bb
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-10-04 10:58:38 -0300

    Add force for one extra frame

--- Assets/Scripts/Player/Backpack.cs
+++ Assets/Scripts/Player/Backpack.cs
@@ -10,11 +10,28 @@ public class Backpack : MonoBehaviour
     public Transform AttachPoint;
     public List<PickableBox> PickableBoxes { get; set; }
 
+    private int _forceFramesRemaining;
+    private Vector2 _force;
+
     private void Awake()
     {
         PickableBoxes = new List<PickableBox>();
         PlayerStats playerStats = GetComponentInParent<PlayerStats>();
         playerStats.DamageReceived.AddListener(DamageRecevied);
+    }
+
+
+    void FixedUpdate()
+    {
+        if (_forceFramesRemaining > 0)
+        {
+            _forceFramesRemaining--;
+            foreach (PickableBox box in PickableBoxes)
+            {
+                Vector2 position = box.Rigidbody2DComponent.position;
+                box.Rigidbody2DComponent.MovePosition(position + _force);
+            }
+        }
     }
 
     private void DamageRecevied(float damage, MonoBehaviour instigator)
@@ -33,6 +50,8 @@ public class Backpack : MonoBehaviour
 
     public void MoveBoxes(Vector2 movement)
     {
+        _force = movement;
+        _forceFramesRemaining = 1;
         Debug.Log("Moving boxes");
         foreach (PickableBox box in PickableBoxes)
         {
