910ee94e9000c5ee25e6132544e0ebf9afe75b15
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-10-03 22:29:22 -0300

    Implement menu and enemy sprite

---
+++ Assets/Scripts/UI/MuteButton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteButton : MonoBehaviour
+{
+    [SerializeField] private GameObject onSprite;
+    [SerializeField] private GameObject offSprite;
+
+    private bool _audioOn;
+
+    private void Awake()
+    {
+        _audioOn = AudioListener.volume > 0.01f;
+        SwitchSprites();
+    }
+
+    public void Toggle()
+    {
+        if (_audioOn)
+        {
+            AudioListener.volume = 0.0f;
+            _audioOn = false;
+        }
+        else
+        {
+            _audioOn = true;
+            AudioListener.volume = 1.0f;
+        }
+        SwitchSprites();
+    }
+
+    private void SwitchSprites()
+    {
+        if (_audioOn)
+        {
+            onSprite.SetActive(true);
+            offSprite.SetActive(false);
+        }
+        else
+        {
+            onSprite.SetActive(false);
+            offSprite.SetActive(true);
+        }
+    }
+
+}
