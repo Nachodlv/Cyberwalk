86ada07de278a10016248e94ebdd464bc0b56376
Nachodlv <ignacio.delavega@ing.austral.edu.ar>
2021-10-04 10:00:52 -0300

    Fix game over screen

--- Assets/Scripts/UI/SceneHelper.cs
+++ Assets/Scripts/UI/SceneHelper.cs
@@ -1,8 +1,14 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneHelper : MonoBehaviour
 {
+    private void Awake()
+    {
+        Time.timeScale = 1.0f;
+    }
+
     public void LoadScene(int sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
